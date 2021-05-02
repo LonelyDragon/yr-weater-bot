3f196c351aa414ef526e29e174a4c05e67456ad8
LonelyDragon <diagnozzz123@gmail.com>
2021-05-02 23:52:08 +0300

    create api handler

---
+++ apps/bot_handlers/api_handler.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+class WeatherData:
+
+    def __init__(self, lat, lon, above = 0) -> None:
+        self.lat = lat
+        self.lon = lon
+        self.above = above
+#https://api.met.no/weatherapi/locationforecast/2.0/complete.json?altitude=11&lat=68.58&lon=33.05
+    def _get_data(self) -> json:
+        url = 'https://api.met.no/weatherapi/locationforecast/2.0/complete.json'
+        r = requests.get(f'{url}?altitude={self.above}&lat={self.lat}&lon={self.lon}')
