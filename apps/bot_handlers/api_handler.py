2bf2ad7c44724044fe8cdcc74cd2e7bbbf6829bd
LonelyDragon <diagnozzz123@gmail.com>
2021-05-26 18:22:22 +0300

    trying get data from yr.no

--- apps/bot_handlers/api_handler.py
+++ apps/bot_handlers/api_handler.py
@@ -11,4 +11,6 @@ class WeatherData:
 #https://api.met.no/weatherapi/locationforecast/2.0/complete.json?altitude=11&lat=68.58&lon=33.05
     def _get_data(self) -> json:
         url = 'https://api.met.no/weatherapi/locationforecast/2.0/complete.json'
-        r = requests.get(f'{url}?altitude={self.above}&lat={self.lat}&lon={self.lon}')
+        headers = {'user-agent': 'YrBot/1.0 github.com/LonelyDragon/yr-weater-bot'}
+        r = requests.get(f'{url}?altitude={self.above}&lat={self.lat}&lon={self.lon}', headers=headers)
+        return r.text
